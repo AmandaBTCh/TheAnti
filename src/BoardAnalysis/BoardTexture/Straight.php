2f2f5698682b6511e4cefc4bfa7283157af96525
Sheldon Juncker <sheldonjuncker@gmail.com>
2017-10-21 17:57:32 -0400

    Finished (tentatively and untested) with the logic for getting straight info.

--- src/BoardAnalysis/BoardTexture/Straight.php
+++ src/BoardAnalysis/BoardTexture/Straight.php
@@ -68,6 +68,60 @@ class Straight extends BoardTexture
 			$threeCombos[] = $cards;
 		}
 
+		/*
+		 * List of made straights where each straight
+		 * is represented by a key with the small card
+		 * and the value of the larger card.
+		 * This automatically ensures we don't have duplicates.
+		 */
+		$straights = [];
 
+		foreach($threeCombos as $threeCombo)
+		{
+			$madeStraights = $this->getComboStraights($threeCombo);
+			foreach($madeStraights as $min => $max)
+			{
+				$straights[$min] = $max;
+			}
+		}
+
+		return count($straights);
+	}
+
+	/*
+	 * Accepts an array of 3 cards and gets an array
+	 * of all possible straights where each straight
+	 * is represented by a key with the min card and
+	 * a value of the max card.
+	 */
+	protected function getComboStraights(array $cards): array
+	{
+		//Minimum and maximum cards in 3 card combo
+		$max = $cards[2];
+		$min = $cards[0];
+
+		//Cards are too far apart, we can't make straights
+		if($max - $min > 4)
+		{
+			return [];
+		}
+
+		//Get the top and bottom range of straights we can make
+		$straightMax = $min + 4;
+		$straightMin = $max - 4;
+
+		//Make sure we're not above or below an Ace
+		$straightMax = min(Card::ACE, $straightMax);
+		$straightMin = max(1, $straightMin);
+
+		$numStraights = ($straightMax - $straightMin) - 3;
+
+		$madeStraights = [];
+		for($i=0; $i<$numStraights; $i++)
+		{
+			$madeStraights[$straightMin + $i] = $straightMin + $i + 4;
+		}
+
+		return $madeStraights;
 	}
 }
